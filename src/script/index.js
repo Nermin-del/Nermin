1e18e3d85d383b5ee5daff92494f3709fa056d6c
Nermin-del <59168189+Nermin-del@users.noreply.github.com>
2020-04-21 16:43:06 +0200

    update

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,8 @@
+/*Jag skapade en funktion som heter showtime. I denna funktion så skapade
+ jag 4 variablar där jag hämtade aktuella datum med new date.
+I varje variabel så skrev jag in timmar, minuter och sekunder med funktionen showTime.
+Jag skapade en variabel också som heter session som är en string som det står "F.M" i. 
+*/ 
 function showTime() {
     var date = new Date();
     var h = date.getHours();
@@ -8,7 +13,7 @@ function showTime() {
     if(h == 0){
         h = 12;
     }
-
+// Om variabel H är större än 12 blir det "=" E.M istället för F.M.
     if(h > 12){
         h = h - 12;
         session = " E.M";
@@ -17,6 +22,11 @@ function showTime() {
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
+// Variabel "var time" sattes här för att programmet ska skilja mellan timmar, minuter och sekunder.
+// "+m" kommer få minuterna att visas på hemsidan
+// "+s" kommer visa sekunderna på hemsidan
+// "+session" visar ifall det är förmiddag eller eftermiddag förkortat "F.M" och "E.M"
+// jag hämtade id clockwork genom document.getElementbyId 
 
     var time = h + ":" + m + ":" + s + session;
     document.getElementById("clockWork").innerText = time;
